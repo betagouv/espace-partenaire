422cd63ae9dd1a42eb83bf0c0521fb98daa09dc9
rebeccadumazert <rebeccadumazert@hotmail.fr>
2023-07-24 15:47:54 +0200

    connecte le front et le back

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  app.use(express.static(path.join(__dirname, '..', 'front', 'build')));
+
   await app.listen(3000);
 }
 bootstrap();
