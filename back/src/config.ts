e4cc174c74d845ca18c22d785989634e48166641
gdarquie <=>
2023-08-02 11:40:20 +0200

    EP-698: debug orm doc and update makefile and doc

--- back/src/config.ts
+++ back/src/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const config = {
   DATABASE_NAME: process.env.DATABASE_NAME || '',
-  DATABASE_USERNAME: process.env.DATABASE_USERNAME || '',
+  DATABASE_USERNAME: process.env.DATABASE_USER || '',
   DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || '',
   DATABASE_HOST: process.env.DATABASE_HOST || '',
   DATABASE_PORT: process.env.DATABASE_PORT
