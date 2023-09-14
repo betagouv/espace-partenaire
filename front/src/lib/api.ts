3a38acada8c64b004c35775697ce96998ecbb337
rebeccadumazert <rebeccadumazert@hotmail.fr>
2023-09-14 15:26:55 +0200

    EP-718/add-environment-variables

--- front/src/lib/api.ts
+++ front/src/lib/api.ts
@@ -5,15 +5,13 @@ const api = {
   getList,
 };
 
-const BASE_URL = 'http://localhost:3000';
-
 async function getKeys() {
-  const response = await axios.get(BASE_URL + '/keys');
+  const response = await axios.get(process.env.REACT_APP_BASE_URL + '/keys');
   return response.data;
 }
 
 async function getList() {
-  const response = await axios.get(BASE_URL + '/list');
+  const response = await axios.get(process.env.REACT_APP_BASE_URL + '/list');
   return response.data;
 }
 
