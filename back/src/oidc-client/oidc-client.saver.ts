3ae197f84b320789e8503627c126aa334cbcd76f
rebeccadumazert <rebeccadumazert@hotmail.fr>
2023-09-28 16:56:18 +0200

    create oidc client saver

--- back/src/oidc-client/oidc-client.saver.ts
+++ back/src/oidc-client/oidc-client.saver.ts
@@ -1,7 +1,25 @@
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateOidcClientDto } from './oidc-client.dto';
+import { OidcClient } from 'src/entities/oidc-client.entity';
+import { Repository } from 'typeorm';
 
 export class OidcClientSaver {
+  constructor(
+    @InjectRepository(OidcClient)
+    private oidcClientRepository: Repository<OidcClient>
+  ) {}
   save = (createOidcClientDto: CreateOidcClientDto): CreateOidcClientDto => {
+    const myOidcClient = new OidcClient();
+    myOidcClient.client_id = createOidcClientDto.clientId;
+    myOidcClient.client_secret = createOidcClientDto.clientSecret;
+    myOidcClient.client_name = createOidcClientDto.clientName;
+    myOidcClient.redirect_uris = createOidcClientDto.redirectUris;
+    myOidcClient.post_logout_redirect_uris =
+      createOidcClientDto.postLogoutRedirectUris;
+    myOidcClient.scope = createOidcClientDto.scope;
+
+    this.oidcClientRepository.save(myOidcClient);
+
     return createOidcClientDto;
   };
 }
