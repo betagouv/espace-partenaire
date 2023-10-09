728c55cff5a41f2f155ef39c6cec5294f775600d
rebeccadumazert <rebeccadumazert@hotmail.fr>
2023-10-09 14:52:14 +0200

    add environment variable back

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,13 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
-  app.use(cors({ origin: 'http://localhost:5173' }));
+  app.use(cors({ origin: process.env.LOCALHOST_URL }));
 
   await app.listen(3000, () => {
     console.log('App listening on port 3000');
