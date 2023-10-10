a3a092561358794ba70dfa7378aa9bead59def20
rebeccadumazert <rebeccadumazert@hotmail.fr>
2023-10-10 15:54:48 +0200

    first tentative to use useContext - WIP

--- front/src/providers/details/ProviderDetails.tsx
+++ front/src/providers/details/ProviderDetails.tsx
@@ -9,6 +9,7 @@ import { ProviderAuthorization } from './ProviderAuthorization';
 import { ProviderUrlDeco } from './ProviderUrlDeco';
 import Title1 from '../../titles/Title1';
 import { fr } from '@codegouvfr/react-dsfr';
+import { createContext, useContext, useState } from 'react';
 
 export type BaseClientOidc = {
   clientName: string;
@@ -20,57 +21,65 @@ export type BaseClientOidc = {
   scope: string[];
 };
 
+const FormDataContext = createContext(null);
+
 export function ProviderDetails() {
-  // const [requestData, setRequestData] = useState<BaseClientOidc>({
-  //   clientName: '',
-  //   clientDescription: '',
-  //   clientId: '',
-  //   clientSecret: '',
-  //   redirectUris: [],
-  //   postLogoutRedirectUris: [],
-  //   scope: [],
-  // });
+  const [formData, setFormData] = useState<BaseClientOidc>({
+    clientName: '',
+    clientDescription: '',
+    clientId: '',
+    clientSecret: '',
+    redirectUris: [],
+    postLogoutRedirectUris: [],
+    scope: [],
+  });
 
   const backgroundBlueFrance =
     fr.colors.decisions.background.alt.blueFrance.default;
 
   return (
-    <div className="fr-container">
-      <Title1 title="Espace de test" />
-      <p>
-        Cette page vous permet de configurer vos test d'intégration du bouton
-        MonComptePro !
-      </p>
-      <div
-        className={`fr-py-6v fr-px-4v`}
-        style={{ backgroundColor: backgroundBlueFrance }}
-      >
+    <FormDataContext.Provider value={{ formData, setFormData }}>
+      <div className="fr-container">
+        <Title1 title="Espace de test" />
         <p>
-          <b className={`fr-h6`}>Documentation</b>
+          Cette page vous permet de configurer vos test d'intégration du bouton
+          MonComptePro !
         </p>
-        <p>
-          Retrouvez toutes les informations dont vous avez besoin et nos
-          <br></br>
-          guidelines graphiques dans l'espace de documentation.
-        </p>
-        <Button iconId="fr-icon-book-2-line" priority="secondary">
-          Lire la documentation
-        </Button>
-      </div>
-      <div className="fr-container--fluid fr-mt-10v">
-        <div className="fr-grid-row">
-          <SideMenu></SideMenu>
-          <div className="fr-col-12 fr-col-md">
-            <ProviderDescription></ProviderDescription>
-            <ProviderKey></ProviderKey>
-            <ProviderUrl></ProviderUrl>
-            <ProviderUrlDeco></ProviderUrlDeco>
-            <ProviderScope></ProviderScope>
-            <ProviderSave></ProviderSave>
-            <ProviderAuthorization></ProviderAuthorization>
+        <div
+          className={`fr-py-6v fr-px-4v`}
+          style={{ backgroundColor: backgroundBlueFrance }}
+        >
+          <p>
+            <b className={`fr-h6`}>Documentation</b>
+          </p>
+          <p>
+            Retrouvez toutes les informations dont vous avez besoin et nos
+            <br></br>
+            guidelines graphiques dans l'espace de documentation.
+          </p>
+          <Button iconId="fr-icon-book-2-line" priority="secondary">
+            Lire la documentation
+          </Button>
+        </div>
+        <div className="fr-container--fluid fr-mt-10v">
+          <div className="fr-grid-row">
+            <SideMenu></SideMenu>
+            <div className="fr-col-12 fr-col-md">
+              <ProviderDescription></ProviderDescription>
+              <ProviderKey></ProviderKey>
+              <ProviderUrl></ProviderUrl>
+              <ProviderUrlDeco></ProviderUrlDeco>
+              <ProviderScope></ProviderScope>
+              <ProviderSave></ProviderSave>
+              <ProviderAuthorization></ProviderAuthorization>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </FormDataContext.Provider>
   );
+}
+
+export function useFormData() {
+  return useContext(FormDataContext);
 }
