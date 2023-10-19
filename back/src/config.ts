179ad11dd62519c0cc071a3920cfddb44b803eb6
rebeccadumazert <rebeccadumazert@hotmail.fr>
2023-10-19 16:34:52 +0200

    add VAR ENV

--- back/src/config.ts
+++ back/src/config.ts
@@ -15,6 +15,7 @@ if (process.env.DATABASE_URL) {
 }
 
 const config = {
+  PORT: Number(process.env.PORT) || 3000,
   DATABASE_NAME: process.env.DATABASE_NAME || '',
   DATABASE_USERNAME: process.env.DATABASE_USER || '',
   DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || '',
