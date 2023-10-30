e4cfe2fb45b9b9e8657bcd97134e8874dfefc297
rebeccadumazert <rebeccadumazert@hotmail.fr>
2023-10-30 17:17:06 +0100

    EP-761 add MuiDsfrThemeProvider

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { startReactDsfr } from '@codegouvfr/react-dsfr/spa';
-import { Router } from './routes/Router'
+import { Router } from './routes/Router';
 import { Link } from 'react-router-dom';
+import { MuiDsfrThemeProvider } from '@codegouvfr/react-dsfr/mui';
 
 declare module '@codegouvfr/react-dsfr/spa' {
   interface RegisterLink {
@@ -17,6 +18,8 @@ startReactDsfr({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <MuiDsfrThemeProvider>
       <Router />
-  </React.StrictMode>,
-)
+    </MuiDsfrThemeProvider>
+  </React.StrictMode>
+);
